9749037bec174d996f17b98a4e579314d2d23934
AurumTheEnd <47597303+AurumTheEnd@users.noreply.github.com>
2021-12-19 10:52:56 +0100

    Derived Debug for Card

--- backend/src/cards/card.rs
+++ backend/src/cards/card.rs
@@ -1,6 +1,6 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Card {
     pub color: CardColor,
     pub symbol: CardSymbol
