11b637c98ec60e94b876943145e267533c1dee92
AurumTheEnd <47597303+AurumTheEnd@users.noreply.github.com>
2022-01-19 14:11:12 +0100

    Added some gamestate tests

--- backend/src/gamestate/mod.rs
+++ backend/src/gamestate/mod.rs
@@ -1,2 +1,71 @@
 pub mod game;
 pub mod player;
+
+#[cfg(test)]
+mod tests {
+    use crate::cards::card::{Card, CardColor, CardSymbol};
+    use crate::gamestate::game::Game;
+    use crate::gamestate::player::Player;
+
+    #[test]
+    fn test_find_player() {
+        let mut game = Game::new("Andy".into());
+        game.add_player("Bob".into());
+
+        assert!(game.find_player("Andy".into()).is_some());
+        assert!(game.find_player("Alice".into()).is_none());
+    }
+
+    #[test]
+    fn test_current_next_players() {
+        let mut game = Game::new("Andy".into());
+        game.add_player("Bob".into());
+
+        let current_player = game.get_current_player();
+        assert!(current_player.is_some());
+        assert_eq!(current_player.unwrap().name, "Andy".to_string());
+
+        game.next_turn();
+        let current_player = game.get_current_player();
+        assert!(current_player.is_some());
+        assert_eq!(current_player.unwrap().name, "Bob".to_string());
+
+        game.next_turn();
+        let current_player = game.get_current_player();
+        assert!(current_player.is_some());
+        assert_eq!(current_player.unwrap().name, "Andy".to_string());
+    }
+
+    #[test]
+    fn test_play_card() {
+        let mut player = Player::new("Chuck".into(), true);
+
+        assert!(player.play_card(0).is_err());
+
+        player.give_card(Card::new(CardColor::Black, CardSymbol::Wild).unwrap());
+
+        assert!(player.play_card(0).is_ok());
+        assert!(player.play_card(1).is_err());
+    }
+
+    #[test]
+    fn test_finished_players() {
+        let mut game = Game::new("Andy".into());
+        game.add_player("Bob".into());
+        game.add_player("Danny".into());
+
+        assert!(game.get_finished_players().is_empty());
+
+        game.players.get_mut(0).unwrap().position = Some(2);
+        game.players.get_mut(1).unwrap().position = Some(1);
+
+        let finished = game.get_finished_players();
+        assert_eq!(
+            finished
+                .into_iter()
+                .map(|p| p.name.clone())
+                .collect::<Vec<String>>(),
+            vec!["Bob".to_string(), "Andy".to_string()]
+        );
+    }
+}
