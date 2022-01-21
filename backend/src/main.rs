0c6df7a18429e4791e08effa18d64aa7ef5e5630
Karolina42 <60822073+Karolina42@users.noreply.github.com>
2022-01-21 15:51:55 +0100

    created server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,23 @@
+use crate::gamestate::game_repo::PostgresGameRepo;
+use actix_web::{web, App, HttpServer};
+
 mod cards;
 mod gamestate;
+mod handlers;
 
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() -> anyhow::Result<()> {
+    let games = Vec::new();
+    let game_repo = (PostgresGameRepo::new(games));
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(game_repo.clone()))
+            .service(handlers::create_game)
+    })
+    .bind("127.0.0.1:9000")?
+    .run()
+    .await?;
+
+    Ok(())
 }
