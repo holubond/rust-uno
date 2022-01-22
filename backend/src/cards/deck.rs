a3460c8c54769debd3aa6f6b4476031e4eb06417
AurumTheEnd <47597303+AurumTheEnd@users.noreply.github.com>
2022-01-22 20:49:35 +0100

    Extracted deck-switching, introduced back-to-black morphing, covered by tests

--- backend/src/cards/deck.rs
+++ backend/src/cards/deck.rs
@@ -45,18 +45,28 @@ impl Deck {
                     return None;
                 }
 
-                let last_discarded_card = self.discard_pile.pop().unwrap();
-
-                self.draw_pile.append(&mut self.discard_pile);
-                self.shuffle_draw_pile();
-
-                self.discard_pile.push(last_discarded_card);
+                self.switch_piles();
 
                 // should definitely be a safe operation
                 Some(self.draw_pile.pop().unwrap())
             }
             Some(card) => Some(card),
         }
+    }
+
+    fn switch_piles(&mut self) {
+        let last_discarded_card = self.discard_pile.pop().unwrap();
+
+        for card in self.discard_pile.iter_mut() {
+            if card.should_be_black() {
+                card.color = CardColor::Black;
+            }
+        }
+
+        self.draw_pile.append(&mut self.discard_pile);
+        self.shuffle_draw_pile();
+
+        self.discard_pile.push(last_discarded_card);
     }
 
     pub fn play(&mut self, card: Card) {
@@ -112,8 +122,9 @@ fn insert_black_symbol_cards(card_stack: &mut Vec<Card>) {
 
 #[cfg(test)]
 mod tests {
-    use crate::cards::deck::Deck;
     use crate::cards::card::{Card, CardColor, CardSymbol};
+    use crate::cards::deck::{Deck, random_color};
+    use rand::Rng;
 
     #[test]
     fn test_card_symbol_eq() {
@@ -157,8 +168,39 @@ mod tests {
     #[test]
     fn test_108_new_cards() {
         let deck = Deck::new();
-        assert_eq!(deck.draw_pile.len(), 108);
+        assert_eq!(deck.draw_pile.len(), 107);
+        assert_eq!(deck.discard_pile.len(), 1);
     }
+
+    #[test]
+    fn test_switch_piles() {
+        use CardColor::*;
+
+        let mut deck = Deck::new();
+
+        for _ in 0..106 {
+            let mut drawn = deck.draw().unwrap();
+            // simulate a full deck being used
+            if drawn.should_be_black() {
+                drawn.color = random_color();
+            }
+
+            deck.play(drawn);
+        }
+
+        let leftover_card = deck.top_discard_card().clone();
+        assert_eq!(deck.discard_pile.len(), 107);
+        assert_eq!(deck.draw_pile.len(), 1); // drawing any more would cause an automatic switch
+        assert!(deck.discard_pile.iter().all(|card| card.color != Black));
+
+        deck.switch_piles();
+
+        assert_eq!(deck.discard_pile.len(), 1);
+        assert_eq!(deck.draw_pile.len(), 107);
+        assert!(deck.draw_pile.iter().all(|card| if card.should_be_black() {card.color == Black} else {true}));
+        assert_eq!(deck.top_discard_card(), &leftover_card);
+    }
+
 }
 
 fn random_color() -> CardColor {
