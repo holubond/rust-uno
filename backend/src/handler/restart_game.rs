ea7baf31b4ec3231e1d5c8beb05784e09c669e13
Karolina42 <60822073+Karolina42@users.noreply.github.com>
2022-01-23 17:01:06 +0100

    added response struct to restart_game

--- backend/src/handler/restart_game.rs
+++ backend/src/handler/restart_game.rs
@@ -4,6 +4,13 @@ use actix_web::{post, web, HttpRequest, HttpResponse, Responder};
 use actix_web_httpauth::headers::authorization::{Authorization, Bearer};
 use std::sync::{Arc, Mutex};
 use actix_web::http::header::Header;
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MessageResponse {
+    message: String,
+}
 
 #[post("game/{gameID}/statusRunning")]
 pub async fn start_game(
@@ -21,7 +28,7 @@ pub async fn start_game(
         .position(|x| x.id == gameID)
         .is_none()
     {
-        return HttpResponse::NotFound();
+        return HttpResponse::NotFound().json(MessageResponse {message:"Game not found".to_string()});
     }
 
     let game_index = data
@@ -33,19 +40,19 @@ pub async fn start_game(
         .unwrap();
 
     if Authorization::<Bearer>::parse(&request).is_err() {
-        return HttpResponse::Unauthorized();
+        return HttpResponse::Unauthorized().json(MessageResponse {message:"No auth token provided by the client".to_string()});
     }
     let jwt = Authorization::<Bearer>::parse(&request)
         .unwrap()
         .to_string();
     let author_name = data.lock().unwrap().games.get(game_index).unwrap().find_author().unwrap().clone().name;
     if !authorization_repo.verify_jwt(author_name,gameID, jwt) {
-        return HttpResponse::Forbidden();
+        return HttpResponse::Forbidden().json(MessageResponse {message:"Token does not prove client is the Author".to_string()});
     }
 
     if data.lock().unwrap().games.get(game_index).unwrap().status == GameStatus::Running {
-        return HttpResponse::Conflict();
+        return HttpResponse::Conflict().json(MessageResponse {message:"Game cannot be started ((re)start is available to games with status LOBBY or FINISHED".to_string()});
     }
 
-    HttpResponse::Ok()
+    HttpResponse::Ok().json(MessageResponse {message: "".to_string()})
 }
