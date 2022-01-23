267e99005869214bf32bfa184d178e4aabb76a10
Were42 <50260518+Were42@users.noreply.github.com>
2022-01-23 16:58:27 +0100

    cards are generated

--- frontend/src/components/myuser.rs
+++ frontend/src/components/myuser.rs
@@ -1,6 +1,10 @@
+use futures::SinkExt;
 use yew::prelude::*;
 use yew::{function_component, html};
 use crate::components::card::{Card, CardProps};
+use crate::Game;
+use crate::pages::game::Msg;
+
 
 pub struct MyUser;
 #[derive(Clone, PartialEq, Properties)]
@@ -18,6 +22,7 @@ impl Component for MyUser {
     }
     fn view(&self, ctx: &Context<Self>) -> Html {
         let cur = ctx.props().current.clone().unwrap();
+        let card = ctx.props().cards.first().unwrap();
         if cur==ctx.props().name.clone() {
             return html!{
                 <div class="flex flex-col w-80 h-96 rounded-lg bg-yellow-300 shadow-md justify-center">
@@ -39,14 +44,14 @@ impl Component for MyUser {
         return html!{
             <div class="flex flex-col w-2/3 h-96 rounded-lg bg-white shadow-md justify-center">
                 <div class="h-80 w-full flex flex-row">
-                        {
-                            ctx.props().cards.iter().map(|card| {
-                                html!{
-                                    <Card color={card.color.clone()} _type={card._type.clone()} value={card.value.clone()} />
-                                }
-                            }).collect::<Html>()
-                        }
-                    </div>
+                    {
+                        ctx.props().cards.iter().map(|card| {
+                            html!{
+                                <Card color={card.color.clone()} _type={card._type.clone()} value={card.value.clone()} />
+                            }
+                        }).collect::<Html>()
+                    }
+                </div>
                 <div>
                     <p class="text-2xl text-center text-Black-500 font-bold">{format!("{}",ctx.props().name)}</p>
                 </div>
