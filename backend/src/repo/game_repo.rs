63ded9ad16068c3bd6ecb2ca3c02595d9332caf2
Karolina42 <60822073+Karolina42@users.noreply.github.com>
2022-01-24 15:42:00 +0100

    added fn for find game by id and games

--- backend/src/repo/game_repo.rs
+++ backend/src/repo/game_repo.rs
@@ -2,11 +2,13 @@ use crate::gamestate::game::Game;
 
 pub trait GameRepo {
     fn add_game(&mut self, name: Game);
+    fn games(&mut self) -> &Vec<Game>;
+    fn find_game_by_id(&self, game_id: String) -> Option<&Game>;
 }
 
 #[derive(Clone)]
 pub struct InMemoryGameRepo {
-    pub(crate) games: Vec<Game>,
+    games: Vec<Game>,
 }
 
 impl InMemoryGameRepo {
@@ -20,5 +22,13 @@ impl InMemoryGameRepo {
 impl GameRepo for InMemoryGameRepo {
     fn add_game(&mut self, game: Game){
         self.games.push(game);
+    }
+
+    fn games(&mut self) -> &Vec<Game>{
+        &self.games
+    }
+
+    fn find_game_by_id(&self, game_id: String) -> Option<&Game>{
+        self.games.iter().find(|game| game.id == game_id)
     }
 }
