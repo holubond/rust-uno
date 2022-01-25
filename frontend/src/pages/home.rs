b1719bee41bf9d7438349d23fcd32f146e7f1ea6
Were42 <50260518+Were42@users.noreply.github.com>
2022-01-25 15:15:45 +0100

    create game fix Ui html

--- frontend/src/pages/home.rs
+++ frontend/src/pages/home.rs
@@ -153,19 +153,18 @@ impl Component for Home {
                                             {"Username"}
                                         </label>
                                     </div>
-                                </div>
 
-                                <div class="md:w-2/3">
-                                    <input
-                                        id="name"
-                                        class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                                        type="text"
-                                        ref={self.name_create.clone()}
-                                        {onchange}
-                                        placeholder="Username"
-                                    />
+                                    <div class="md:w-2/3">
+                                        <input
+                                            id="name"
+                                            class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                                            type="text"
+                                            ref={self.name_create.clone()}
+                                            {onchange}
+                                            placeholder="Username"
+                                        />
+                                    </div>
                                 </div>
-
 
                                 <div class="md:flex md:items-center">
                                     <div class="md:w-1/3">
