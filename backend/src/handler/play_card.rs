c37d19cf62d8a3231af6fdd887ab66d459f30495
Were42 <50260518+Were42@users.noreply.github.com>
2022-01-25 12:01:40 +0100

    adding error handling for end point

--- backend/src/handler/play_card.rs
+++ backend/src/handler/play_card.rs
@@ -1,12 +1,17 @@
 use std::sync::{Arc, Mutex};
 use actix_web::{HttpResponse, Responder, web};
-use crate::{AddressRepo, InMemoryGameRepo};
+use crate::{AddressRepo, AuthorizationRepo, InMemoryGameRepo};
 use crate::cards::card::{Card, CardColor};
+use crate::gamestate::game::GameStatus;
+
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct PlayCardData {
     card: Card,
-    newColor: CardColor
+    #[serde(rename(serialize = "newColor", deserialize = "new_color"))]
+    new_color: CardColor,
+    #[serde(rename(serialize = "saidUno", deserialize = "said_uno"))]
+    said_uno: bool
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -17,14 +22,37 @@ pub struct MessageResponse {
 #[post("/game/{gameID}/playCard")]
 pub async fn create_game(
     game_repo: web::Data<Arc<Mutex<InMemoryGameRepo>>>,
+    authorization_repo: web::Data<Arc<AuthorizationRepo>>,
     address_repo: web::Data<Arc<AddressRepo>>,
     body: web::Json<PlayCardData>,
     params: web::Path<String>,
 ) -> impl Responder {
-    let gameID = params.into_inner();
+    let game_id = params.into_inner();
+    let card = &body.card;
+    let new_color = body.new_color;
+    let said_uno = body.said_uno;
+    let mut game_repo = game_repo.lock().unwrap();
 
-    let game = match game_repo.lock().unwrap().find_game_by_id(gameID.clone()).clone() {
-        Some(game) => game.clone(),
+    let game = match game_repo.find_game_by_id(&game_id) {
+        Some(game) => game,
         _=> return HttpResponse::NotFound().json(MessageResponse {message:"Game not found".to_string()})
     };
+    let jwt = authorization_repo.parse_jwt(request);
+    let jwt = match jwt {
+        Ok(jwt) => jwt.to_string(),
+        _ => return HttpResponse::Unauthorized().json(MessageResponse {message:"No auth token provided by the client".to_string()})
+    };
+    let claims = match authorization_repo.valid_jwt(&jwt)  {
+        Ok(claims) => claims,
+        _ => return HttpResponse::Unauthorized().json(MessageResponse {message:"Token is not valid".to_string()})
+    };
+    let username = authorization_repo.user_from_claims(&claims);
+
+    let player = match game.find_player(username.clone()) {
+        Some(player) => player,
+        _ => return HttpResponse::InternalServerError().json(ErrorMessageResponse{message: "Game does not have player".to_string()})
+    };
+    if !authorization_repo.verify_jwt(username.clone(),game_id, claims) {
+        return HttpResponse::Forbidden().json(ErrorMessageResponse {message:"Token does not prove client is the Author".to_string()});
+    }
 }
