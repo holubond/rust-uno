1435e3f5465b89ee2277f2613a52cbafaa416107
Ondra Holub <ondrejholub202@gmail.com>
2022-01-25 03:21:30 +0100

    Prefix unused params with _

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -12,15 +12,15 @@ impl Component for App {
     type Message = ();
     type Properties = ();
 
-    fn create(ctx: &Context<Self>) -> Self {
+    fn create(_ctx: &Context<Self>) -> Self {
         Self
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
         false
     }
 
-    fn view(&self, ctx: &Context<Self>) -> Html {
+    fn view(&self, _ctx: &Context<Self>) -> Html {
         return html! {
                 <BrowserRouter>
                     <Switch<Route> render={Switch::render(switch)} />
