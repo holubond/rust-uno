22ef1a2360d9aff26efe36149f4a4136da97f98c
Ondra Holub <ondrejholub202@gmail.com>
2022-01-29 14:11:10 +0100

    Remove unused code

--- backend/src/handler/service/auth.rs
+++ backend/src/handler/service/auth.rs
@@ -1,6 +1,6 @@
 use actix_web::http::header::Header;
 use actix_web::HttpRequest;
-use actix_web::{error::ParseError, HttpResponse};
+use actix_web::HttpResponse;
 use actix_web_httpauth::headers::authorization::{Authorization, Bearer};
 use jwt_simple::prelude::*;
 use crate::handler::util::response::ErrMsg;
@@ -82,38 +82,6 @@ impl AuthService {
         };
         let claims = Claims::with_custom_claims(jwt_data, Duration::from_days(9000));
         self.key.authenticate(claims).unwrap()
-    }
-
-    pub fn valid_jwt(&self, token: &String) -> Result<JWTClaims<JwtData>, anyhow::Error> {
-        let token = self.rem_bearer(&token);
-        self.key.verify_token::<JwtData>(&token, None)
-    }
-
-    pub fn verify_jwt(
-        &self,
-        player_name: String,
-        game_id: String,
-        claims: JWTClaims<JwtData>,
-    ) -> bool {
-        let is_author = claims.custom.game_id == game_id;
-        let is_user = claims.custom.player_name == player_name;
-        is_author && is_user
-    }
-
-    pub fn user_from_claims(&self, claims: &JWTClaims<JwtData>) -> String {
-        claims.custom.player_name.clone()
-    }
-
-    pub fn parse_jwt(&self, request: HttpRequest) -> Result<Authorization<Bearer>, ParseError> {
-        Authorization::<Bearer>::parse(&request)
-    }
-
-    fn rem_bearer(&self, value: &str) -> String {
-        let mut chars = value.chars();
-        for _ in 0..7 {
-            chars.next();
-        }
-        chars.as_str().to_string()
     }
 
     fn remove_bearer_prefix(&self, auth_bearer: String) -> Result<String, HttpResponse> {
