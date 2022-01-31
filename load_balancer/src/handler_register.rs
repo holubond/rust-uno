3c38a3ecc32e5e6bb84b1c0cd286ebde816349be
Ondra Holub <ondrejholub202@gmail.com>
2022-01-31 15:04:31 +0100

    Save server from the data it sends

--- load_balancer/src/handler_register.rs
+++ load_balancer/src/handler_register.rs
@@ -1,32 +1,19 @@
-use actix_web::{put, web, HttpRequest, HttpResponse};
+use actix_web::{put, web, HttpResponse};
 use serde::Deserialize;
 
 use crate::game_server_repo::{AddGameServerResult, GameServerRepo};
 
 #[derive(Deserialize, Debug)]
 pub struct RequestBody {
-    port: String,
+    server: String,
 }
 
 #[put("/gameServer")]
 pub async fn register_game_server(
-    request: HttpRequest,
     body: web::Json<RequestBody>,
     game_server_repo: web::Data<GameServerRepo>,
 ) -> HttpResponse {
-
-    let conn_info = request.connection_info();
-    let ip_with_outgoing_port = match conn_info.realip_remote_addr() {
-        None => return HttpResponse::InternalServerError().body("Cannot resolve IP from request"),
-        Some(ip) => ip,
-    };
-
-    let ip = match get_receiver_addr(ip_with_outgoing_port, body.port.clone()) {
-        Err(response) => return response,
-        Ok(ip) => ip,
-    };
-
-    game_server_repo.add(ip).into()
+    game_server_repo.add(body.server.clone()).into()
 }
 
 impl From<AddGameServerResult> for HttpResponse {
@@ -39,16 +26,4 @@ impl From<AddGameServerResult> for HttpResponse {
             ServerAdded => HttpResponse::Created().finish(),
         }
     }
-}
-
-fn get_receiver_addr(ip_port: &str, port: String) -> Result<String, HttpResponse> {
-
-    let ip = match ip_port.split(":").next() {
-        None => return Err(HttpResponse::InternalServerError().body(
-            "Error when splitting IP"
-        )),
-        Some(x) => x,
-    };
-
-    Ok(format!("{}:{}", ip, port))
 }
